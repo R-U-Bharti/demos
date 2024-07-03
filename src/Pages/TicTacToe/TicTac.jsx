26df92a14971914dcfcf9546f5fe14d0051a5b38
R-U-Bharti <kumar.bharti@gtropy.com>
2024-07-03 15:15:28 +0530

    app

--- src/Pages/TicTacToe/TicTac.jsx
+++ src/Pages/TicTacToe/TicTac.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import ReactDOMServer from 'react-dom/server';
 import toast from 'react-hot-toast';
+import android from './android.png'
 import './TicTac.css'
 
 const TicTac = () => {
@@ -444,6 +445,16 @@ const TicTac = () => {
                     </div>
                 </div>
             </dialog>
+
+            <div className='w-full fixed bottom-4 flex justify-center'>
+                <a href={'./TicTacToe.apk'} download class="bg-green-950 text-green-300 border border-green-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+                    <span class="bg-green-100 shadow-green-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+                    <div className='flex gap-2 items-center'>
+                        <img src={android} className='w-[5vw] md:w-[2vw]' alt="" srcset="" />
+                        <span>Download APK</span>
+                    </div>
+                </a>
+            </div>
 
         </>
     )
