c8427343b18abd5d2db99018c30e42587aeb125b
R-U-Bharti <kumar.bharti@gtropy.com>
2024-09-24 13:30:34 +0530

    ch

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,16 +8,16 @@ const Home = () => {
 
     const projects = [
         { image: tictactoe, name: "TicTacToe", path: "/tic-tac-toe" },
-        { image: genAi, name: "Gen AI", path:"/gen-ai" },
+        { image: genAi, name: "Gen AI", path: "/gen-ai" },
         { image: imageAnalyser, name: "AI Image Analyser", path: "/image-analyser" },
     ]
 
     return (
-        <div className="flex flex-wrap gap-4 md:px-8 justify-center px-2 md:py-4 py-2 animate__animated animate__fadeIn">
-
-            <div className="w-full flex justify-center">
-                <h1 className='text-2xl font-medium text-center py-2 mt-2 mb-4 border-b border-gray-400 w-max px-10'>Entertaining Projects By <span className="font-bold text-amber-400 hover:underline cursor-pointer" onClick={() => window.open('https://r-u-bharti.github.io/portfolio', '_blank')}>R U Bharti</span></h1>
-            </div>
+        <div className="h-screen">
+            <div className='flex flex-wrap gap-4 md:px-8 justify-center place-items-start px-2 md:py-4 py-2 animate__animated animate__fadeIn'>
+                <div className="w-full flex justify-center">
+                    <h1 className='text-2xl font-medium text-center py-2 mt-2 mb-4 border-b border-gray-400 w-max px-10'>Entertaining Projects By <span className="font-bold text-amber-400 hover:underline cursor-pointer" onClick={() => window.open('https://r-u-bharti.github.io/portfolio', '_blank')}>R U Bharti</span></h1>
+                </div>
 
                 {
                     projects?.map((elem, index) =>
@@ -25,6 +25,7 @@ const Home = () => {
                     )
                 }
 
+            </div>
         </div>
     )
 }
